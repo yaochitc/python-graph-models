f4dde2a159aa271f7348bd24997545311aba02c1
yaochitc <yaochi@sugo.io>
2019-03-11 17:27:05 +0800

    add methods for negative sampling

--- util/data_util.py
+++ util/data_util.py
@@ -29,21 +29,86 @@ def load_kg(kg_file):
 
 
 def construct_kg(kg_np):
-    kg = collections.defaultdict(list)
+    kg = dict()
     for head, relation, tail in kg_np:
-        kg[head].append((tail, relation))
+        if head not in kg:
+            kg[head] = collections.defaultdict(list)
+        kg[head][relation].append(tail)
     return kg
 
 
-def get_tails(kg, n_relation, item):
-    relations_dict = collections.defaultdict(list)
-    for tail, relation in kg[item]:
-        relations_dict[relation].append(tail)
+def get_tails(kg, n_relation, items):
+    def _get_tails(item):
+        tails = []
+        for relation in range(n_relation):
+            if relation in kg[item]:
+                tails.append(np.random.choice(kg[item][relation]))
+            else:
+                tails.append(-1)
+        return tails
 
     tails = []
-    for relation in range(n_relation):
-        if relation in relations_dict:
-            tails.append(np.random.choice(relations_dict[relation]))
+    for item in items:
+        tails.append(_get_tails(item))
+    return np.array(tails)
+
+
+def sample_kg(kg, n_relation, items):
+    relation_tails = collections.defaultdict(set)
+    for item in kg.keys():
+        for relation in range(n_relation):
+            relation_tails[relation].update(kg[item][relation])
+
+    subset = dict()
+    def _get_sample_list(item, relation, subset):
+        if item not in subset:
+            subset[item] = dict()
+
+        if relation not in subset[item]:
+            subset[item][relation] = list(relation_tails[relation] - set(kg[item][relation]))
+        return subset[item]
+
+    def _sample_kg(item):
+        tails = []
+        for relation in range(n_relation):
+            sample_list = _get_sample_list(item, relation, subset)
+
+            if len(sample_list) == 0:
+                tails.append(-1)
+            else:
+                tails.append(np.random.choice(list(sample_list)))
+
+        return tails
+
+    tails = []
+    for item in items:
+        tails.append(_sample_kg(item))
+    return np.array(tails)
+
+def sample_ratings(ratings, users):
+    rating_items = collections.defaultdict(set)
+    item_set = set()
+    for u, i in ratings:
+        rating_items[u].add(i)
+        item_set.add(i)
+
+    subset = dict()
+
+    def _get_sample_list(user, subset):
+        if user not in subset:
+            subset[user] = list(item_set - set(rating_items[user]))
+        return subset[user]
+
+    def _sample_ratings(user):
+        sample_list = _get_sample_list(user, subset)
+
+        if len(sample_list) == 0:
+            return tails.append(-1)
         else:
-            tails.append(-1)
-    return tails
+            return np.random.choice(sample_list)
+
+    tails = []
+    for user in users:
+        tails.append(_sample_ratings(user))
+    return np.array(tails)
+
